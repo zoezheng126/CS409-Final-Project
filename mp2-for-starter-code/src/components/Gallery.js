843bdd0d5ad12a3e500727cfdade3c14568809e1
PassbyKamenRider <PassbyKamenRider@outlook.com>
2022-11-27 17:20:41 -0600

    Add mp2 for starter code

---
+++ mp2-for-starter-code/src/components/Gallery.js
@@ -0,0 +1,90 @@
+import './Gallery.css';
+import {Link} from "react-router-dom";
+import {useState, React} from 'react';
+import PropTypes from 'prop-types';
+
+function Gallery({allPokemons}) {
+    const [pokemonFiltered, setpokemonFiltered] = useState([]);
+
+    const handleFilterChange = (event) => {
+        setpokemonFiltered({pokemonFiltered : allPokemons.sort((a, b) => a.id - b.id)});
+        if (event.target.value === "showAll") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons});
+        } else if (event.target.value === "1-200") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(0,200)});
+        } else if (event.target.value === "201-400") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(200,400)});
+        } else if (event.target.value === "401-600") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(400,600)});
+        } else if (event.target.value === "601-800") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(600,800)});
+        } else if (event.target.value === "801-1000") {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(800,1000)});
+        } else {
+            setpokemonFiltered({pokemonFiltered :
+                allPokemons.slice(1000,1154)});
+        }
+    }
+
+    return (
+        <div>
+            <div className="header">
+            <img src={require('../pokeapi.png')} alt="loading"/>
+            </div>
+            <div className="navbar">
+            <div className="navbar_item1">
+                <Link to="/">
+                <div className="navbar_text">
+                    Search
+                </div>
+                </Link>
+            </div>
+            <div className="navbar_item2">
+                <Link to="/gallery">
+                <div className="navbar_text">
+                    Gallery
+                </div>
+                </Link>
+            </div>
+            </div>
+
+            <div className="galleryNavBar">
+                <button type="button" value="showAll" onClick={handleFilterChange}> show all </button>
+                <button type="button" value="1-200" onClick={handleFilterChange}> 1-200 </button>
+                <button type="button" value="201-400" onClick={handleFilterChange}> 201-400 </button>
+                <button type="button" value="401-600" onClick={handleFilterChange}> 401-600 </button>
+                <button type="button" value="601-800" onClick={handleFilterChange}> 601-800 </button>
+                <button type="button" value="801-1000" onClick={handleFilterChange}> 801-1000 </button>
+                <button type="button" value="1001-1154" onClick={handleFilterChange}> 1001-1154 </button>
+            </div>
+
+            <div className="searchContent">
+                {pokemonFiltered.length !== 0 ? (
+                    (pokemonFiltered["pokemonFiltered"]).map((pokemon) => (
+                        <Link to = {`/details/${pokemon.id}`}>
+                            <div key={pokemon.id} className="searchElement">
+                                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="loading"/>
+                                <div className="galleryFont"> {pokemon.name} </div>
+                            </div>
+                        </Link>
+                    ))
+                ) : (
+                    <div> </div>
+                )}
+            </div>
+
+        </div>
+    );
+}
+
+Gallery.propTypes = {
+    allPokemons: PropTypes.array
+}
+
+export default Gallery;
---
+++ mp2-for-starter-code/src/components/Search.js
@@ -0,0 +1,124 @@
+import './Search.css';
+import {Link} from "react-router-dom";
+import {useState, React} from 'react';
+import PropTypes from 'prop-types';
+
+function PokemonSearch({allPokemons}) {
+    const [pokemonFiltered, setpokemonFiltered] = useState([]);
+    const [sortOrder, setSortOrder] = useState("ascending");
+    const [sortBy, setsortBy] = useState("id");
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setpokemonFiltered({pokemonFiltered :
+        allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(value.toLowerCase()))});
+    };
+
+    const handleSortChange = (event) => {
+        setSortOrder({sortOrder : event.target.value});
+        if (sortBy["sortBy"] === "id") {
+            if (event.target.value === "ascending") {
+                setpokemonFiltered({pokemonFiltered:
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => a.id - b.id)});
+            } else {
+                setpokemonFiltered({pokemonFiltered:
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => b.id - a.id)});
+            }
+        } else {
+            if (event.target.value === "ascending") {
+                setpokemonFiltered({pokemonFiltered:
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => (a.name < b.name ? -1 : 1))});
+            } else {
+                setpokemonFiltered({pokemonFiltered:
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => (a.name < b.name ? 1 : -1))});
+            }
+        }
+    }
+
+    const handleSortByChange = (event) => {
+        setsortBy({sortBy : event.target.value});
+        if (sortOrder["sortOrder"] === "ascending") {
+            if (event.target.value === "id") {
+                setpokemonFiltered({pokemonFiltered :
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => a.id - b.id)});
+            } else {
+                setpokemonFiltered({pokemonFiltered :
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => (a.name < b.name ? -1 : 1))});
+            }
+        } else {
+            if (event.target.value === "id") {
+                setpokemonFiltered({pokemonFiltered :
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => b.id - a.id)});
+            } else {
+                setpokemonFiltered({pokemonFiltered :
+                pokemonFiltered["pokemonFiltered"].sort((a, b) => (a.name < b.name ? 1 : -1))});
+            }
+        }
+    }
+
+    return (
+        <div>
+            <div className="header">
+            <img src={require('../pokeapi.png')} alt="loading"/>
+            </div>
+
+            <div className="navbar">
+            <div className="navbar_item1">
+                <Link to="/">
+                <div className="navbar_text">
+                    Search
+                </div>
+                </Link>
+            </div>
+            <div className="navbar_item2">
+                <Link to="/gallery">
+                <div className="navbar_text">
+                    Gallery
+                </div>
+                </Link>
+            </div>
+            </div>
+
+            <input type="text" class="searchForm" onChange={handleInputChange} placeholder="Enter Pokemon Name..." />
+
+            <div className="sortForm">
+                <input type="radio" id="ascending" name="sortOrder" value="ascending" onChange={handleSortChange}/>
+                <label for="ascending"> ascending </label>
+                <input type="radio" id="descending" name="sortOrder" value="descending" onChange={handleSortChange}/>
+                <label for="descending"> descending </label>
+
+                <br/>
+
+                sort by
+
+                <select onChange={handleSortByChange}>
+                    <option value="name">name</option>
+                    <option value="id">id</option>
+                </select>
+            </div>
+
+            <div className="searchContent">
+                {pokemonFiltered.length !== 0 ? (
+                    (pokemonFiltered["pokemonFiltered"]).map((pokemon) => (
+                        <Link to={`/details/${pokemon.id}`}>
+                            <div key={pokemon.id} className="searchElement">
+                                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="loading"/>
+                                <div className="searchFont"> {pokemon.name} </div>
+                            </div>
+                        </Link>
+                    ))
+                ) : (
+                    <div> </div>
+                )}
+            </div>
+
+        </div>
+    );
+}
+
+PokemonSearch.propTypes = {
+    allPokemons: PropTypes.array
+}
+
+export default PokemonSearch;
